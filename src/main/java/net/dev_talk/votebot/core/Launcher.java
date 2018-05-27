b362393e32df6277145709b513bc215bff854aea
LGA1151 <kahnkilian@a1.net>
2018-05-27 19:53:24 +0200

    Initial Commit

---
+++ src/main/java/net/dev_talk/votebot/core/Launcher.java
@@ -0,0 +1,10 @@
+package net.dev_talk.votebot.core;
+
+/**
+ * @author LGA1151 (https://github.com/LGA1151)
+ */
+public class Launcher {
+    public static void main(final String[] args) {
+        final VoteBot bot = new VoteBot();
+    }
+}
---
+++ src/main/java/net/dev_talk/votebot/core/VoteBot.java
@@ -0,0 +1,6 @@
+package net.dev_talk.votebot.core;
+
+/**
+ * @author LGA1151 (https://github.com/LGA1151)
+ */
+public class VoteBot {}
